65e5898b6f83fa0dbbc542a31811b7d91feab035
xiongyx <123456>
2018-07-26 10:24:59 +0800

    爬虫工具类更新

--- util.py
+++ util.py
@@ -1,19 +1,24 @@
 import urllib.request
 
-header = {
-   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
-}
 
-def download(url,retryCount=2):
+
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
+
+def download(url,user_agent=user_agent,retryCount=2):
 	print('download: ' + url);
 	try:
+		header = {
+			'User-Agent': user_agent
+		}	
+	
 		request = urllib.request.Request(url,headers=header);
 		html = urllib.request.urlopen(request).read();
 		return html;
 	except urllib.error.HTTPError as e:
 		print(e.reason);
 		
-		if (retryCount > 0) and (hasattr(e,"code")) and (500<=e.code and e.code<=599):
+		if (retryCount > 0) 
+			if (hasattr(e,"code")) and (500<=e.code and e.code<=599):
 			print('retry download ===== code=' + str(e.code));
 			download(url,retryCount-1);
 		else:
