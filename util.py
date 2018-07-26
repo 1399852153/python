af781ac271cca4501385f7bf56e3740d23ff9ffe
xiongyx <123456>
2018-07-26 10:35:34 +0800

    bug修复

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import urllib.request
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
 
 def download(url,user_agent=user_agent,retryCount=2):
-	print('download: ' + url);
+	print('download: ' + url + 'retryCount=' + str(retryCount));
 	try:
 		header = {
 			'User-Agent': user_agent
@@ -19,8 +19,8 @@ def download(url,user_agent=user_agent,retryCount=2):
 		
 		if (retryCount > 0):
 			if (hasattr(e,"code")) and (500<=e.code and e.code<=599):
-				print('retry download ===== code=' + str(e.code));
-				return download(url,retryCount-1);
+				print('retry download ===== code=' + str(e.code) + " retryCount=" + str(retryCount));
+				return download(url,retryCount=retryCount-1);
 			else:
 				return None;
 		else:
